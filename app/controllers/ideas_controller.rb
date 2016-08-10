27348b1e5f8576d7245444d1bfa4c8e5dfb736c8
sthpravin <sthpravin@gmail.com>
2016-08-10 23:18:46 +0545

    Created a form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,5 +2,17 @@ class IdeasController < ApplicationController
 	def index
 		@ideas = Idea.all
 	end
+
+	def create
+			@idea = Idea.create(idea_params)
+			redirect_to root_path
+	end
+
+	private
+
+	def idea_params
+		params.require(:idea).permit(:description, :author)
+		
+	end
 end
 
